3ca3ccf27272a2a7d9a83fa54aa338d00e64520f
stephenitis <nguyenhstephen@gmail.com>
2013-05-06 12:34:15 -0700

    update/create models for twitter project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,29 @@
 class User < ActiveRecord::Base
+  has_many :tweets
+  
   attr_reader :entered_password
 
-  validates :first_name, :length => { :minimum => 3, :message => "must be at least 3 characters" }
-  validates :last_name, :length => { :minimum => 3, :message => "must be at least 3 characters" }
-  validates :user_name, :length => { :minimum => 3, :message => "must be at least 3 characters" }
-  validates :entered_password, :length => { :minimum => 6 }
-  validates :email, :uniqueness => true, :format => /.+@.+\..+/ # imperfect, but okay
+  # validates :first_name, :length => { :minimum => 3, :message => "must be at least 3 characters" }
+  # validates :last_name, :length => { :minimum => 3, :message => "must be at least 3 characters" }
+  # validates :user_name, :length => { :minimum => 3, :message => "must be at least 3 characters" }
+  # validates :entered_password, :length => { :minimum => 6 }
+  # validates :email, :uniqueness => true, :format => /.+@.+\..+/ # imperfect, but okay
 
   include BCrypt
+
+  def tweets_scale?
+    
+  end
+
+
+
+  def fetch_tweets!
+    fetched_tweets = Twitter.user_timeline(self.twitter_handle).map {|tweet| tweet.text}
+    fetched_tweets.each do |t| 
+      self.tweets << Tweet.create(:text => t)
+    end
+    self.save
+  end
 
   def password
     @password ||= Password.new(password_hash)
